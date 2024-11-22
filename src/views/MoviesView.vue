0189efd994205dca6490f4069ecf32b6cb22ec56
Rafabitte1 <rafabitte1@gmail.com>
2024-11-22 08:50:08 -0300

    listando

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,45 +1,48 @@
 <script setup>
   import { ref, onMounted } from 'vue';
   import api from '@/plugins/axios';
+ import Loading from 'vue-loading-overlay';
+ import { useGenreStore } from '@/stores/genre';
+ import { useRouter } from 'vue-router'
+ const router = useRouter()
 
+const genreStore = useGenreStore();
   const genres = ref([]);
 
+
+  function getGenreName(id) {
+    const genero = genres.value.find((genre) => genre.id === id);
+    return genero.name;
+  }
+
   onMounted(async () => {
-    const response = await api.get('genre/movie/list?language=pt-BR');
-    genres.value = response.data.genres;
-  });
+  isLoading.value = true;
+  await genreStore.getAllGenres('movie');
+  isLoading.value = false;
+});
 
   const movies = ref([]);
 
-  const listMovies = async (genreId) => {
-      const response = await api.get('discover/movie', {
-          params: {
-              with_genres: genreId,
-              language: 'pt-BR'
-          }
-      });
-      movies.value = response.data.results
-  };
-  import Loading from 'vue-loading-overlay';
+  
+
 
 const isLoading = ref(false);
 const listMovies = async (genreId) => {
+  genreStore.setCurrentGenreId(genreId);
   isLoading.value = true;
   const response = await api.get('discover/movie', {
     params: {
       with_genres: genreId,
-      language: 'pt-BR'
-    }
+      language: 'pt-BR',
+    },
   });
-  movies.value = response.data.results
+  movies.value = response.data.results;
   isLoading.value = false;
 };
-
-const getGenreName = (id) => genres.value.find((genre) => genre.id === id).name
-function getGenreName(id) {
-    const genero = genres.value.find((genre) => genre.id === id);
-    return genero.name;
-  }
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
+function openMovie(movieId) {
+  router.push({ name: 'MovieDetails', params: { movieId } });
+}
 </script>
 <template>
    <h1>Filmes</h1>
@@ -50,40 +53,61 @@ function getGenreName(id) {
   </ul>
   <loading v-model:active="isLoading" is-full-page />
   <li
-    v-for="genre in genres"
+    v-for="genre in genreStore.genres"
     :key="genre.id"
     @click="listMovies(genre.id)"
     class="genre-item"
+    :class="{ active: genre.id === genreStore.currentGenreId }"
   >
+  
     {{ genre.name }}
+  
   </li>
+
+  
   <div class="movie-list">
   <div v-for="movie in movies" :key="movie.id" class="movie-card">
     
     <img
-      :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
-      :alt="movie.title"
-    />
+  :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+  :alt="movie.title"
+  @click="openMovie(movie.id)"
+/>
     <div class="movie-details">
       <p class="movie-title">{{ movie.title }}</p>
       <p class="movie-release-date">{{ movie.release_date }}</p>
       <p class="movie-genres">{{ movie.genre_ids }}</p>
     </div>
-    
+
+
   </div>
 </div>
 <p class="movie-genres">
-  <span
-    v-for="genre_id in movie.genre_ids"
-    :key="genre_id"
-    @click="listMovies(genre_id)"
-  >
-    {{ getGenreName(genre_id) }} 
-  </span>
-</p>
+ 
+ <span
+v-for="genre_id in movie.genre_ids"
+:key="genre_id"
+@click="listMovies(genre_id)"
+:class="{ active: genre_id === genreStore.currentGenreId }"
+>
+{{ genreStore.getGenreName(genre_id) }} 
+</span>
+
+</p>;
+<p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
 </template>
 
 <style scoped>
+.active {
+  background-color: #67b086;
+  font-weight: bolder;
+}
+
+.movie-genres span.active {
+  background-color: #abc322;
+  color: #000;
+  font-weight: bolder;
+}
 .movie-genres {
   display: flex;
   flex-direction: row;
