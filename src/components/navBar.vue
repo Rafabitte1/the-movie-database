4e36ceb32e118d4c53990de6d9fdf4ea2c402d08
Rafabitte1 <rafabitte1@gmail.com>
2024-12-16 14:35:43 -0300

    refazendo

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,14 +1,38 @@
 <template>
     <header>
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/filmes">Filmes</router-link>
+            <router-link to="/tv">Séries</router-link>
+        </nav>
     </header>
 </template>
 
 <style scoped>
 header {
-    background-color: #2c3e50; 
+    background-color: #001830; 
     color: #ffffff; 
     padding: 1rem;
     text-align: center;
+    text-shadow: #001830;
+    padding-right: 20px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); 
+}
+
+nav {
+    display: flex;
+    justify-content: center; 
+    column-gap: 2rem;
+}
+
+nav a {
+    text-decoration: none;
+    color: #ffffff; 
+    font-weight: bold; 
+    transition: color 0.3s; 
+}
+
+nav a:hover {
+    color: #f39c12; 
 }
 </style>
